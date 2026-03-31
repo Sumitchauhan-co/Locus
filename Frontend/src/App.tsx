330174e4e86eced0cdc9039d7f6cd73dbb020cb6
Sumitchauhan-co <chauhan.sumit3012@gmail.com>
2026-04-01 02:12:24 +0530

    v3.5.3

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,40 +2,40 @@ import React, { useContext, useEffect, useState } from 'react';
 import './App.css';
 import AppRoute from './routes/AppRoute';
 import { AuthContext } from './contexts/AuthContext';
-// import Loader from './components/Loader';
-// import Loader2 from './components/Loader2';
 import Loader3 from './components/Loader3';
 import Container from './components/LayoutContainer';
 import Loading2 from './components/Loading2';
 
 function App(): React.ReactNode {
     const [showApp, setShowApp] = useState(false);
-    const [isLandscape, setIsLandscape] = useState(false);
+    const [isLandscapeMobile, setIsLandscapeMobile] = useState(false);
+
     const { loading, getUser } = useContext(AuthContext);
 
     useEffect(() => {
         getUser();
     }, []);
 
-    
     useEffect(() => {
         const checkOrientation = () => {
-            setIsLandscape(window.innerWidth > window.innerHeight);
+            const isMobile = window.innerWidth <= 768;
+            const isLandscape = window.innerWidth > window.innerHeight;
+
+            setIsLandscapeMobile(isMobile && isLandscape);
         };
 
         checkOrientation();
 
         window.addEventListener('resize', checkOrientation);
-
         return () => window.removeEventListener('resize', checkOrientation);
     }, []);
 
-    if (isLandscape) {
+    if (isLandscapeMobile) {
         return (
             <Container>
                 <div className="min-h-screen w-full flex justify-center flex-col items-center gap-3">
                     <Loading2 />
-                    <span className="text-white animate-pulse">
+                    <span className="text-white animate-pulse text-lg">
                         Rotate back!
                     </span>
                 </div>
@@ -43,6 +43,7 @@ function App(): React.ReactNode {
         );
     }
 
+    // 🔹 Loader logic
     if (loading || !showApp) {
         return <Loader3 onFinish={() => setShowApp(true)} />;
     }
