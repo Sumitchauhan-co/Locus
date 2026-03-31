9a4e8a2fccea49f7b7accf43bfe5e0c7e94015a1
Sumitchauhan-co <chauhan.sumit3012@gmail.com>
2026-04-01 03:15:36 +0530

    v3.5.3.2

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,10 +1,9 @@
 import express from "express";
 import authController from "../controllers/auth.controller.js";
-import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const route = express.Router()
 
-route.get("/user", authMiddleware, authController.getUser)
+route.get("/user", authController.getUser)
 
 route.post("/register", authController.registerUser)
 
