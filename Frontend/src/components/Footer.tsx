268db4ba4baa7908d8ae9c38ce5052b212691427
Sumitchauhan-co <chauhan.sumit3012@gmail.com>
2026-04-01 11:11:41 +0530

    v3.5.5

--- Frontend/src/components/Footer.tsx
+++ Frontend/src/components/Footer.tsx
@@ -6,26 +6,26 @@ const Footer: React.FC = () => {
     const navigate = useNavigate();
 
     return (
-        <footer className="h-fit w-full p-5 flex relative z-9999 bottom-0 bg-(--secondary-color)">
+        <footer className="h-fit w-full p-10 pb-20 flex relative z-9999 bottom-0 bg-(--secondary-color)">
             <div className="w-1/2 flex justify-center items-center">
                 <SiReactivex className="h-10 w-10" />
             </div>
             <div className="w-1/2 flex md:flex-row md:gap-0 gap-3 flex-col justify-around items-center">
                 <span
                     onClick={() => navigate('/contact')}
-                    className="cursor-pointer text-sm hover:underline text-center"
+                    className="cursor-pointer sm:text-[1rem] text-sm hover:underline text-center"
                 >
                     Contact
                 </span>
                 <span
                     onClick={() => navigate('/')}
-                    className="cursor-pointer text-sm hover:underline text-center"
+                    className="cursor-pointer sm:text-[1rem] text-sm hover:underline text-center"
                 >
                     Privacy Policy
                 </span>
                 <span
                     onClick={() => navigate('/')}
-                    className="cursor-pointer text-sm hover:underline text-center"
+                    className="cursor-pointer sm:text-[1rem] text-sm hover:underline text-center"
                 >
                     Terms & Conditions
                 </span>
